88646cba285fe93227f8e820fee2e25d80e5e80a
Keerthan M P <keerthankeeru775@gmail.com>
2023-06-30 12:29:28 +0530

    trapping rain water

---
+++ two pointers/trapping_rain_water.java
@@ -0,0 +1,23 @@
+//two pointers
+//tc O(n), sc O(1)
+class Solution {
+    public int trap(int[] height) {
+        if(height.length==0){
+            return 0;
+        }
+        int l=0, r=height.length-1;
+        int leftMax=height[l], rightMax=height[r];
+        int res=0;
+        while(l<r){
+            if(leftMax<rightMax){
+                leftMax=Math.max(leftMax,height[++l]);
+                res += leftMax-height[l];
+            }
+            else{
+                rightMax=Math.max(rightMax, height[--r]);
+                res += rightMax-height[r];
+            }
+        }
+        return res;
+    }
+}
