53cd77d3677a0ae1869f6e479f47fbca48cf0b2a
Keerthan M P <keerthankeeru775@gmail.com>
2023-07-03 07:02:15 +0530

    search 2d matrix

---
+++ binary search/search_2d_matrix.java
@@ -0,0 +1,39 @@
+public class search_2d_matrix {
+    
+}
+// tc O(log(m+n)), sc O(1)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int ROWS = matrix.length, COLS = matrix[0].length;
+        int top = 0, bot = ROWS-1, row = 0;
+        while(top <= bot){
+            row = (top + bot) / 2;
+            if(target > matrix[row][COLS-1]){
+                top = row + 1;
+            }
+            else if(target < matrix[row][0]){
+                bot = row - 1;
+            }
+            else{
+                break;
+            }
+        }
+        if(!(top <= bot)){
+            return false;
+        }
+        int l = 0, r = COLS-1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(target < matrix[row][mid]){
+                r = mid - 1;
+            }
+            else if(target > matrix[row][mid]){
+                l = mid + 1;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
