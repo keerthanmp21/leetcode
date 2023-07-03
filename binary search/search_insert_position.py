cde38997b9ff13e6e7c3fd28d900fcc8dd6d6af2
Keerthan M P <keerthankeeru775@gmail.com>
2023-07-03 06:25:37 +0530

    search insert position

---
+++ binary search/search_insert_position.py
@@ -0,0 +1,13 @@
+# tc O(logn), sc O(1)
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
