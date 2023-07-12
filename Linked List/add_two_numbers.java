b37698c48adde51c4f1a391361fbbf3dc2a8fce5
Keerthan M P <keerthankeeru775@gmail.com>
2023-07-12 07:00:21 +0530

    changes in add two numbers

--- Linked List/add_two_numbers.java
+++ Linked List/add_two_numbers.java
@@ -1,19 +1,19 @@
-
-//Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
- 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
         ListNode cur = dummy;
         int carry = 0;
-        while(l1!=null || l2!=null || carry==1){
+        while(l1!=null || l2!=null || carry!=0){
             int l1_val = 0;
             if(l1!=null){
                 l1_val = l1.val;
