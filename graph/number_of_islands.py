391b211c98e233cc2408338ed929c52a1be035cd
Keerthan M P <keerthankeeru775@gmail.com>
2023-07-16 19:44:20 +0530

    number of islands

---
+++ graph/number_of_islands.py
@@ -0,0 +1,62 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    # dfs
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return
+
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+        def dfs(r,c):
+            if (r not in range(ROWS) or 
+                c not in range(COLS) or 
+                grid[r][c] != '1' or
+                (r,c) in visited):
+                return
+            visited.add((r,c))
+            for d in directions:
+                dfs(r+d[0],c+d[1])
+
+        islands = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    islands += 1
+                    dfs(r,c)
+
+        return islands
+        
+    # bfs
+    def numIslands2(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return
+
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+        def bfs(r,c):
+            q = deque()
+            q.append([r,c])
+            while q:
+                r,c = q.popleft()
+                if (r in range(ROWS) and
+                c in range(COLS) and
+                grid[r][c] == '1' and
+                (r,c) not in visited):
+                    visited.add((r,c))
+                    for d in directions:
+                        q.append([r+d[0], c+d[1]])
+
+        islands = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    islands += 1
+                    bfs(r,c)
+
+        return islands
--- graph/surrounded_regions.py
+++ graph/surrounded_regions.py
@@ -1,6 +1,8 @@
 from typing import List
+from collections import deque
 
 class Solution:
+    # dfs
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
@@ -32,3 +34,27 @@ class Solution:
             for c in range(COLS):
                 if board[r][c] == 'T':
                     board[r][c] = 'O'
+    
+    # bfs
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        q = deque([])
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r in [0,ROWS-1] or c in [0,COLS-1]) and board[r][c] == 'O':
+                    q.append((r,c))
+        
+        while q:
+            r, c = q.popleft()
+            if 0<=r<ROWS and 0<=c<COLS and board[r][c] == 'O':
+                board[r][c] = 'T'
+                q.extend(((r+1,c),(r-1,c),(r,c+1),(r,c-1)))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                if board[r][c] == 'T':
+                    board[r][c] = 'O'
+                
