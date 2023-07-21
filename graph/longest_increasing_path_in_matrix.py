79e17d509f8949f0c123de5559a2d3127496db1e
Keerthan M P <keerthankeeru775@gmail.com>
2023-07-21 19:47:59 +0530

    longest increasing path in matrix

---
+++ graph/longest_increasing_path_in_matrix.py
@@ -0,0 +1,54 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        dp = {}
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        maxValue = [0]
+
+        def dfs(r,c,preValue):
+            if r<0 or r==ROWS or c<0 or c==COLS or matrix[r][c]<=preValue:
+                return 0
+            if (r,c) in dp:
+                return dp[(r,c)]
+            res = 1
+            for d in directions:
+                res = max(res,1+dfs(r+d[0],c+d[1],matrix[r][c]))
+            dp[(r,c)] = res
+            maxValue[0] = max(maxValue[0],res)
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c,-1)
+        return maxValue[0]
+    
+    # bfs (tle)
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        if ROWS == 1 and COLS == 1:
+            return 1
+
+        def bfs(r,c):
+            q = deque()
+            q.append((r,c))
+            res = 0
+            while q:
+                res += 1
+                for _ in range(len(q)):
+                    row, col = q.popleft()
+                    for d in directions:
+                        dr, dc = row+d[0], col+d[1]
+                        if(dr<0 or dr==ROWS) or (dc<0 or dc==COLS) or matrix[dr][dc] > matrix[r][c]:
+                            continue
+                        q.append((dr,dc))
+            return res
+
+        maxValue = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                maxValue = max(maxValue, bfs(r,c))
+        return maxValue
