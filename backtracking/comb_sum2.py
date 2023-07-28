c0019c1eca4a14b7016d09fa14afb94d985fa44c
Keerthan M P <keerthankeeru775@gmail.com>
2023-07-28 07:16:31 +0530

    combination sum2 and two sum2-input array sorted

---
+++ backtracking/comb_sum2.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res=[]
+        candidateLen = len(candidates)
+        def backtrack(pos, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if total > target:
+                return 
+            prev = -1
+            for i in range(pos,candidateLen):
+                if candidates[i] == prev:
+                    continue
+                cur.append(candidates[i])
+                backtrack(i+1, cur, total+candidates[i])
+                cur.pop()
+                prev = candidates[i]
+        backtrack(0, [], 0)
+        return res
--- two pointers/two_sum2_input_array_sorted.py
+++ two pointers/two_sum2_input_array_sorted.py
@@ -1,7 +1,9 @@
+from typing import List
+
 # binary search
 # tc O(n), sc O(1)
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum2(self, numbers: List[int], target: int) -> List[int]:
         l, r = 0, len(numbers)-1
         while l<r:
             curSum = numbers[l]+numbers[r]
@@ -12,3 +14,17 @@ class Solution:
             else:
                 return [l+1, r+1]
         return []
+    
+    def twoSum(self, numbers, target):
+        N = len(numbers)
+        for i in range(N):
+            l, r = i+1, N-1
+            tmp = target - numbers[i]
+            while l <= r:
+                mid = l + (r-l)//2
+                if numbers[mid] == tmp:
+                    return [i+1, mid+1]
+                elif numbers[mid] < tmp:
+                    l = mid+1
+                else:
+                    r = mid-1
