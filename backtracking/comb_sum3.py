0398ee698453b56f6eef719d65d4cb59fe35a2b3
Keerthan M P <keerthankeeru775@gmail.com>
2023-07-29 15:57:19 +0530

    combination sum3

---
+++ backtracking/comb_sum3.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        result = []
+
+        def dfs(i, subset):
+            if i > 10:
+                return
+            if len(subset) == k:
+                if sum(subset) == n:
+                    return result.append(subset.copy())
+                else:
+                    return
+            
+            subset.append(i)
+            dfs(i+1, subset)
+            subset.pop()
+            dfs(i+1, subset)
+
+
+        dfs(1,[])
+
+        return result
+            
