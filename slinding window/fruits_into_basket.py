718d60de94af4f173094262834994f495e7ee9f4
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-03 18:37:22 +0530

    fruits into basket

---
+++ slinding window/fruits_into_basket.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        l = 0
+        output = 0
+        d = {}
+        for r in range(len(fruits)):
+            if fruits[r] in d:
+                d[fruits[r]] += 1
+            else:
+                if len(d) == 2:
+                    output = max(output,sum(d.values()))
+                    while len(d) == 2:
+                        d[fruits[l]] -= 1
+                        if d[fruits[l]] == 0:
+                            del d[fruits[l]]
+                        l += 1
+                d[fruits[r]] = 1
+        return max(output,sum(d.values()))
