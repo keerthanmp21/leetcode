0eb8877dd4b489cea604dc38b7db5f69b05e6c89
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-03 14:51:00 +0530

    few changes

--- dp/longest_valid_parentheses.py
+++ dp/longest_valid_parentheses.py
@@ -1,23 +1,26 @@
 class Solution:
     # stack
     # tc O(n), sc O(n)
-    def longestValidParentheses(self, s: str) -> int:
+    def longestValidParentheses2(self, s: str) -> int:
+        '''
+        we only update the result(max) when we find a "pair".
+        If we find a pair. We throw this pair away and see how big the gap is 
+        between current and previous invalid.
+        '''
         stack = [-1]
         res = 0
         for i in range(len(s)):
-            if s[i] == '(':
-                stack.append(i)
-            else:
+            if s[i] == ')' and len(stack) > 1 and s[stack[-1]] == '(':
                 stack.pop()
-                if len(stack) == 0:
-                    stack.append(i)
-                else:
-                    res = max(res,i-stack[-1])
+                res = max(res, i-stack[-1])
+            else:
+                stack.append(i)
         return res
         
-    #dp tabulation
+        
+    # dp tabulation
     # tc O(n), sc O(n)
-    def longestValidParentheses2(self, s: str) -> int:
+    def longestValidParentheses(self, s: str) -> int:
         N = len(s)
         dp = [0]*N
         res = 0
@@ -25,10 +28,15 @@ class Solution:
         for i in range(N):
             if s[i] == '(':
                 leftCount += 1
-            elif leftCount>0:
+            elif leftCount > 0:
+                leftCount -= 1
                 dp[i] = dp[i-1]+2
                 if (i-dp[i]) >= 0:
                     dp[i] += dp[i-dp[i]]
                 res = max(res, dp[i])
-                leftCount -= 1
+                
+        print(dp)
         return res    
+
+s = Solution()
+print(s.longestValidParentheses('()(())'))
