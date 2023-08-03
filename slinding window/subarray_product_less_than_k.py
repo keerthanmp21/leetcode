56f3e6ee2a5375e7bd1d5d93b1efe8c90af62dd7
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-03 16:37:33 +0530

    subarray product less than k

---
+++ slinding window/subarray_product_less_than_k.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        l = 0
+        total = 1
+        res = 0
+
+        for r in range(len(nums)):
+            total = total * nums[r]
+            while l<len(nums) and total >= k:
+                total = total/nums[l]
+                l += 1
+            if total < k:
+                res += (r-l+1)
+
+        return max(res, 0)
