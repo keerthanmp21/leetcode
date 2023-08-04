ef57a3246cc5a92534283001a201d3a0c9e9e1fc
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-04 15:42:12 +0530

    maximum erasure value

---
+++ slinding window/max_erasure_value.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+        subArraySum = 0
+        
+        for r in range(len(nums)):
+            while nums[r] in charSet:
+                charSet.remove(nums[l])
+                subArraySum -= nums[l]
+                l += 1
+            charSet.add(nums[r])
+            subArraySum += nums[r]
+            res = max(res, subArraySum)
+        return res
