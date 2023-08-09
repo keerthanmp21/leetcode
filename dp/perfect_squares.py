81d655e7b487609b5f592908b775a50e11fbd818
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-09 07:13:54 +0530

    few changes

--- dp/perfect_squares.py
+++ dp/perfect_squares.py
@@ -1,50 +1,53 @@
-# backtrack recursion
-# tc O(n^n), sc O(1)
+from typing import List
+
+
 class Solution:
-    def numSquares(self, n: int) -> int:
+    # backtrack recursion
+    # tc O(n^n), sc O(1)
+    def numSquares1(self, n: int) -> int:
         def backtrack(num):
-            if num<4:
+            if num < 4:
                 return num
             res = num
-            for i in range(1,num+1):
-                square = i*i
-                if square<=num:
-                    res = min(res,1+backtrack(num-square))
+            for i in range(1, num + 1):
+                square = i * i
+                if square <= num:
+                    res = min(res, 1 + backtrack(num - square))
             return res
+
         return backtrack(n)
 
-# dp memoization
-# tc O(n^2), sc O(n)
-class Solution2:
-    def numSquares(self, n: int) -> int:
+    # dp memoization
+    # tc O(n^2), sc O(n)
+    def numSquares2(self, n: int) -> int:
         cache = {}
+
         def backtrack(num):
-            if num<4:
+            if num < 4:
                 return num
             if num in cache:
                 return cache[num]
             res = num
-            for i in range(1,num+1):
-                square = i*i
-                if square<=num:
-                    res = min(res,1+backtrack(num-square))
+            for i in range(1, num + 1):
+                square = i * i
+                if square <= num:
+                    res = min(res, 1 + backtrack(num - square))
             cache[num] = res
             return cache[num]
+
         return backtrack(n)
-        
-# dp tabulation
-# tc O(n^2), sc O(n)
-class Solution2:
-    def numSquares(self, n: int) -> int:
-        dp = [n]*(n+1)
+
+    # dp tabulation
+    # tc O(n^2), sc O(n)
+    def numSquares3(self, n: int) -> int:
+        dp = [n] * (n + 1)
         dp[0] = 0
-        
-        for target in range(1,n+1):
-            for s in range(1,target+1):
-                square = s*s
-                if target - square <0:
+
+        for target in range(1, n + 1):
+            for s in range(1, target + 1):
+                square = s * s
+                if target - square < 0:
                     break
-                dp[target] = min(dp[target], 1+dp[target-square])
-                
+                dp[target] = min(dp[target], 1 + dp[target - square])
+
         return dp[n]
-        
