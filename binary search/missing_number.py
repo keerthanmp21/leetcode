97b2fad8a5273c3751a6f82d22fb939801a034e6
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-10 07:20:52 +0530

    few changes

--- binary search/missing_number.py
+++ binary search/missing_number.py
@@ -1,25 +1,25 @@
 from typing import List
 
-# greedy
-# tc O(n), sc O(1)
+
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+    # greedy
+    # tc O(n), sc O(1)
+    def missingNumber1(self, nums: List[int]) -> int:
         res = len(nums)
 
         for i in range(len(nums)):
             res += i - nums[i]
         return res
 
-# binary search
-# tc O(nlogn), sc O(1)
-class Solution2:
-    def missingNumber(self, nums: List[int]) -> int:
+    # binary search
+    # tc O(nlogn), sc O(1)
+    def missingNumber2(self, nums: List[int]) -> int:
         nums.sort()
-        l, r = 0, len(nums)-1
+        l, r = 0, len(nums) - 1
         while l <= r:
             mid = (l + r) // 2
             # number is missing b/w mid and r
-            if nums[mid] == mid: 
+            if nums[mid] == mid:
                 l = mid + 1
             else:
                 r = mid - 1
