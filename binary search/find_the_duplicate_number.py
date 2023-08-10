b021bc982fe2c1705558987133342f3a0dd7fab6
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-10 16:13:15 +0530

    find the duplicate number

--- binary search/find_the_duplicate_number.py
+++ binary search/find_the_duplicate_number.py
@@ -2,7 +2,39 @@ from typing import List
 
 
 class Solution:
+    # counting
+    # tc O(n), sc O(n)
     def findDuplicate1(self, nums: List[int]) -> int:
+        count = [0] * len(nums)
+
+        for n in nums:
+            count[n] += 1
+            if count[n] > 1:
+                return n
+
+    # hash table
+    # tc O(n), sc O(n)
+    def findDuplicate2(self, nums: List[int]) -> int:
+        visitSet = set()
+        
+        for n in nums:
+            if n in visitSet:
+                return n
+            visitSet.add(n)
+
+    # sorting
+    # tc O(nlogn), sc O(logn)
+    def findDuplicate3(self, nums: List[int]) -> int:
+        nums.sort()
+
+        for i in range(0, len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return nums[i]
+        
+
+    # two pointers
+    # tc O(n), sc O(1)
+    def findDuplicate4(self, nums: List[int]) -> int:
         slow, fast = 0, 0
         while True:
             slow = nums[slow]
@@ -17,7 +49,14 @@ class Solution:
             if slow == slow2:
                 return slow
 
-    def findDuplicate2(self, nums: List[int]) -> int:
+    # binary search
+    # tc O(nlogn), sc O(1)
+    def findDuplicate5(self, nums: List[int]) -> int:
+        '''
+        We can use the binary search algorithm, each round we guess one number, then scan the input array, narrow the search range, and finally get the answer.
+
+According to the Pigeonhole Principle, n+1n + 1n+1 integers, placed in an array of length nnn, at least 111 integer will be repeated.
+        '''
         l, r = 0, len(nums) - 1
         while l < r:
             mid = (l + r) // 2
