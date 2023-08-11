5650ca6cc31957d2dc67462d2a95eef69b9948d5
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-11 07:10:58 +0530

    few changes

--- two pointers/find_the_index_of_the_first_occurence_of_string.py
+++ two pointers/find_the_index_of_the_first_occurence_of_string.py
@@ -1,29 +1,28 @@
-# brute force
-# tc O(m*n), sc O(1)
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack)-len(needle)+1):
+    # brute force
+    # tc O(m*n), sc O(1)
+    def strStr1(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack) - len(needle) + 1):
             for j in range(len(needle)):
-                if haystack[i+j] != needle[j]:
+                if haystack[i + j] != needle[j]:
                     break
-                if j == len(needle)-1:
+                if j == len(needle) - 1:
                     return i
         return -1
 
-# two pointers
-# tc O(m), sc O(1) 
-class Solution2:
-    def strStr(self, haystack: str, needle: str) -> int:
+    # two pointers
+    # tc O(m), sc O(1)
+    def strStr2(self, haystack: str, needle: str) -> int:
         l1, l2, r1, r2 = 0, 0, len(haystack), len(needle)
-        while l1<r1:
+        while l1 < r1:
             if l2 == r2:
-                return l1-l2
+                return l1 - l2
             if haystack[l1] == needle[l2]:
                 l2 += 1
             else:
-                l1 = l1-l2
+                l1 = l1 - l2
                 l2 = 0
             l1 += 1
-        if l2==r2:
-            return l1-l2
+        if l2 == r2:
+            return l1 - l2
         return -1
