c3be2fc70c17c37916630996c259b16036b72e8a
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-11 07:47:55 +0530

    rotate array

--- two pointers/rotate_array.py
+++ two pointers/rotate_array.py
@@ -1,8 +1,10 @@
 from typing import List
-# brute force
-# tc O(kn), sc O(n)
+
+
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+    # brute force
+    # tc O(kn), sc O(n)
+    def rotate1(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -11,21 +13,21 @@ class Solution:
             nums[1:] = nums[0:-1]
             nums[0] = last_ele
 
-# two pointers
-# tc O(n), sc O(1)
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+    # two pointers
+    # tc O(n), sc O(1)
+    def rotate2(self, nums: List[int], k: int) -> None:
         N = len(nums)
-        def rotateNums(l,r):
-            while l<r:
+
+        def rotateNums(l, r):
+            while l < r:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
                 r -= 1
-            
-        if k>N:
-            k = k%N
-        
-        if k>0:
-            rotateNums(0,N-1)
-            rotateNums(0,k-1)
-            rotateNums(k,N-1)
+
+        if k > N:
+            k = k % N
+
+        if k > 0:
+            rotateNums(0, N - 1)
+            rotateNums(0, k - 1)
+            rotateNums(k, N - 1)
