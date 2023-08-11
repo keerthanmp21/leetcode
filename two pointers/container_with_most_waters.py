d2f7feb5d11c9675112fd451382610636b153a60
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-11 06:56:16 +0530

    few changes

--- two pointers/container_with_most_waters.py
+++ two pointers/container_with_most_waters.py
@@ -1,24 +1,26 @@
-#brute force
-# tc O(n^2), sc O(n)
-class Solution1:
-    def maxArea(self, height: List[int]) -> int:    
+from typing import List
+
+
+class Solution:
+    # brute force
+    # tc O(n^2), sc O(n)
+    def maxArea1(self, height: List[int]) -> int:
         res = 0
-        for i in range(len(height)-1):
-            for j in range(i+1, len(height)):
-                area = (j-i)*min(height[i], height[j])
+        for i in range(len(height) - 1):
+            for j in range(i + 1, len(height)):
+                area = (j - i) * min(height[i], height[j])
                 res = max(res, area)
         return res
 
-# two pointers or greedy
-# tc O(n), sc O(n)
-class Solution2:
-    def maxArea(self, height: List[int]) -> int:
-        l, r = 0, len(height)-1
+    # two pointers or greedy
+    # tc O(n), sc O(n)
+    def maxArea2(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
         res = 0
-        while l<r:
-            area = (r-l)*min(height[l], height[r])
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
             res = max(res, area)
-            if height[l]<height[r]:
+            if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
