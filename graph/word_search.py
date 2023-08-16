0491d945f33efe4b589883b2f4034d5121673832
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-16 16:46:19 +0530

    word search

---
+++ graph/word_search.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (
+                r < 0
+                or c < 0
+                or r >= ROWS
+                or c >= COLS
+                or word[i] != board[r][c]
+                or (r, c) in path
+            ):
+                return False
+            path.add((r, c))
+            res = False
+            for dr, dc in directions:
+                res = res or dfs(r + dr, c + dc, i + 1)
+            path.remove((r, c))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+
+        return False
