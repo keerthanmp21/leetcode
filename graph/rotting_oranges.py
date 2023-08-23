cc2a3c77542b1b7492925e947ce1a8ce00762cb1
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-23 06:20:41 +0530

    rotting oranges

--- graph/rotting_oranges.py
+++ graph/rotting_oranges.py
@@ -1,6 +1,7 @@
 from typing import List
 from collections import deque
 
+
 class Solution:
     # bfs
     def orangesRotting(self, grid: List[List[int]]) -> int:
@@ -12,19 +13,25 @@ class Solution:
                 if grid[r][c] == 1:
                     fresh += 1
                 elif grid[r][c] == 2:
-                    q.append((r,c))
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                    q.append((r, c))
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-        while q and fresh>0:
+        while q and fresh > 0:
             for _ in range(len(q)):
-                r,c = q.popleft()
+                r, c = q.popleft()
                 for dr, dc in directions:
-                    row, col = r+dr, c+dc
-                    if row<0 or row==ROWS or col<0 or col==COLS or grid[row][col] != 1:
+                    row, col = r + dr, c + dc
+                    if (
+                        row < 0
+                        or row == ROWS
+                        or col < 0
+                        or col == COLS
+                        or grid[row][col] != 1
+                    ):
                         continue
                     fresh -= 1
                     grid[row][col] = 2
-                    q.append((row,col))
+                    q.append((row, col))
             time += 1
 
         return time if fresh == 0 else -1
