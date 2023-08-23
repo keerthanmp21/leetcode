245cfb1971cf4fee43a6858470fc7e03471e5953
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-23 06:26:00 +0530

    pacific atlantic water flow

--- graph/pacific_atlantic_water_flow.py
+++ graph/pacific_atlantic_water_flow.py
@@ -1,54 +1,60 @@
 from typing import List
 from collections import deque
 
+
 class Solution:
     # dfs
     # tc O(m*n), sc O(m*n)
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+    def pacificAtlantic1(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
         pac, atl = set(), set()
-        
+
         def dfs(r, c, visit, prevHeight):
-            if((r,c) in visit or r<0 or c<0 or r==ROWS or c==COLS or 
-              heights[r][c]<prevHeight):
+            if (
+                (r, c) in visit
+                or r < 0
+                or c < 0
+                or r == ROWS
+                or c == COLS
+                or heights[r][c] < prevHeight
+            ):
                 return
-            visit.add((r,c))
-            dfs(r+1, c, visit, heights[r][c])
-            dfs(r-1, c, visit, heights[r][c])
-            dfs(r, c+1, visit, heights[r][c])
-            dfs(r, c-1, visit, heights[r][c])
-            
+            visit.add((r, c))
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
+
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
-            dfs(ROWS-1, c, atl, heights[ROWS-1][c])
-            
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+
         for r in range(ROWS):
             dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS-1, atl, heights[r][COLS-1])
-            
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+
         res = []
         for r in range(ROWS):
             for c in range(COLS):
-                if (r,c) in pac and (r,c) in atl:
-                    res.append([r,c])
-                    
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+
         return res
-    
+
     # bfs
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+    def pacificAtlantic2(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
         pacific = []
         atlantic = []
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         for r in range(ROWS):
-            pacific.append((r,0))
-            atlantic.append((r,COLS-1))
+            pacific.append((r, 0))
+            atlantic.append((r, COLS - 1))
         for c in range(COLS):
-            pacific.append((0,c))
-            atlantic.append((ROWS-1,c))
-        
-        
+            pacific.append((0, c))
+            atlantic.append((ROWS - 1, c))
+
         def bfs(l):
             q = deque(l)
             visit = set(l)
@@ -57,9 +63,14 @@ class Solution:
                 for d in directions:
                     dr = r + d[0]
                     dc = c + d[1]
-                    if (0 <= dr < ROWS) and (0 <= dc < COLS) and (dr,dc) not in visit and heights[dr][dc] >= heights[r][c]:
-                        visit.add((dr,dc))
-                        q.append((dr,dc))
-            return visit 
-        
+                    if (
+                        (0 <= dr < ROWS)
+                        and (0 <= dc < COLS)
+                        and (dr, dc) not in visit
+                        and heights[dr][dc] >= heights[r][c]
+                    ):
+                        visit.add((dr, dc))
+                        q.append((dr, dc))
+            return visit
+
         return bfs(pacific) & bfs(atlantic)
