863aaee47e4436bb12244e93307f698260f86ec7
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-24 07:31:15 +0530

    few changes

--- graph/open_the_lock.py
+++ graph/open_the_lock.py
@@ -3,27 +3,29 @@ from collections import deque
 
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        if '0000' in deadends:
+        if "0000" in deadends:
             return -1
-        
+
+        q = deque()
+        q.append(("0000", 0))
+        visit = set(deadends)
+
         def children(lock):
             res = []
             for i in range(4):
-                digit = str((int(lock[i])+1)%10)
-                res.append(lock[:i]+digit+lock[i+1:])
-                digit = str((int(lock[i])-1+10)%10)
-                res.append(lock[:i]+digit+lock[i+1:])
+                digit = str((int(lock[i]) + 1) % 10)
+                res.append(lock[:i] + digit + lock[i + 1 :])
+                digit = str((int(lock[i]) - 1 + 10) % 10)
+                res.append(lock[:i] + digit + lock[i + 1 :])
             return res
-        
-        q = deque()
-        q.append(["0000",0])
-        visit = set(deadends)
+
         while q:
             lock, turns = q.popleft()
             if lock == target:
                 return turns
             for child in children(lock):
                 if child not in visit:
+                    q.append((child, turns + 1))
                     visit.add(child)
-                    q.append([child,turns+1])
+
         return -1
