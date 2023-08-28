d43e56ec2d70e370256967f880e7fc896b03cd63
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-28 14:42:09 +0530

    maximum no of vowels in substring of given length

--- slinding window/max_no_of_vowels_in_substring_of_given_length.py
+++ slinding window/max_no_of_vowels_in_substring_of_given_length.py
@@ -6,11 +6,11 @@ class Solution:
         numOfVowels = 0
         res = 0
         for r in range(len(s)):
-            if s[r] in 'aeiou':
+            if s[r] in "aeiou":
                 numOfVowels += 1
-            if (r-l+1) == k:
+            if (r - l + 1) == k:
                 res = max(res, numOfVowels)
-                if s[l] in 'aeiou':
+                if s[l] in "aeiou":
                     numOfVowels -= 1
                 l += 1
         return res
