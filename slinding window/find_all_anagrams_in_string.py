b919c26a4004d97e38955719ddff61e82d249ab0
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-28 14:32:02 +0530

    find all anagrams in string

--- slinding window/find_all_anagrams_in_string.py
+++ slinding window/find_all_anagrams_in_string.py
@@ -1,27 +1,30 @@
+from typing import List
+
+
 # tc O(n), sc O(n)
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         lenP = len(p)
         lenS = len(s)
-        if lenP>lenS:
+        if lenP > lenS:
             return []
         pCount = {}
         sCount = {}
         for i in range(lenP):
-            pCount[p[i]] = pCount.get(p[i],0)+1
-            sCount[s[i]] = sCount.get(s[i],0)+1
+            pCount[p[i]] = pCount.get(p[i], 0) + 1
+            sCount[s[i]] = sCount.get(s[i], 0) + 1
 
         l = 0
         res = [0] if pCount == sCount else []
 
-        for r in range(lenP,lenS):
-            sCount[s[r]] = sCount.get(s[r],0)+1
+        for r in range(lenP, lenS):
+            sCount[s[r]] = sCount.get(s[r], 0) + 1
             sCount[s[l]] -= 1
             if sCount[s[l]] == 0:
-                #sCount.pop(s[l])
+                # sCount.pop(s[l])
                 del sCount[s[l]]
             l += 1
             if sCount == pCount:
                 res.append(l)
 
-        return res   
+        return res
