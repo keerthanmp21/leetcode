4fa8516717c79bb406d8ace183ac3f2a2ad73d95
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-28 14:40:11 +0530

    maximum average subarray1

--- slinding window/maximum_average_subarray1.py
+++ slinding window/maximum_average_subarray1.py
@@ -1,16 +1,30 @@
+from typing import List
+
+
 # sliding window
 # tc O(n), sc O(1)
 class Solution:
-    def findMaxAverage(self, nums: List[int], k: int) -> float:
+    def findMaxAverage1(self, nums: List[int], k: int) -> float:
         l = 0
-        res = float('-inf')
+        res = float("-inf")
         total = 0
         for r in range(len(nums)):
-            if r<k:
+            if r < k:
                 total += nums[r]
             else:
                 res = max(res, total)
-                total += nums[r]-nums[l]
-                l+=1
-        res = max(res,total)
-        return res/k
+                total += nums[r] - nums[l]
+                l += 1
+        res = max(res, total)
+        return res / k
+
+    def findMaxAverage2(self, nums: List[int], k: int) -> float:
+        l = 0
+        total = sum(nums[:k])
+        res = total
+        for r in range(k, len(nums)):
+            res = max(res, total)
+            total += nums[r] - nums[l]
+            l += 1
+        res = max(res, total)
+        return res / k
