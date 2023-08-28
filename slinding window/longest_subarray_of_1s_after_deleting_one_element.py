926378e12d9a7169ba9a69d7b751416ac8ed1948
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-28 14:52:41 +0530

    longest subarray of 1's after deleting one element

--- slinding window/longest_subarray_of_1s_after_deleting_one_element.py
+++ slinding window/longest_subarray_of_1s_after_deleting_one_element.py
@@ -1,8 +1,11 @@
+from typing import List
+
+
 # tc O(n), sc O(1)
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         l = res = 0
-        k = 1 # max zero we can delete
+        k = 1  # max zero we can delete
 
         for r in range(len(nums)):
             # decrement k if we encounter 0
@@ -11,11 +14,11 @@ class Solution:
 
             # if we have seen more than 1 zero then move l
             # till we discard first zero
-            while k<0 and l<=r:
+            while k < 0 and l <= r:
                 if nums[l] == 0:
                     k += 1
                 l += 1
 
-            res = max(res,r-l)
+            res = max(res, r - l)
 
         return res
