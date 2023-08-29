c0040900cd10eef282691cce46457e354e8b7074
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-29 18:54:43 +0530

    few changes

--- two pointers/max_num_of_ksum_pairs.py
+++ two pointers/max_num_of_ksum_pairs.py
@@ -1,18 +1,21 @@
+from typing import List
+
+
 # two pointers and sorting
 # tc O(nlogn), sc O(1)
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         nums.sort()
-        l, r = 0, len(nums)-1
+        l, r = 0, len(nums) - 1
         total_count = 0
-        while l<r:
-            _sum = nums[l]+nums[r]
-            if _sum<k:
-                l+=1
-            elif _sum>k:
-                r-=1
+        while l < r:
+            _sum = nums[l] + nums[r]
+            if _sum < k:
+                l += 1
+            elif _sum > k:
+                r -= 1
             else:
                 total_count += 1
-                l+=1
-                r-=1
+                l += 1
+                r -= 1
         return total_count
