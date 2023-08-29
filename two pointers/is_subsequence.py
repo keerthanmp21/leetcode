791ea29d26b456ffe573085baf94bd2fefd1a157
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-29 18:53:27 +0530

    few changes

--- two pointers/is_subsequence.py
+++ two pointers/is_subsequence.py
@@ -3,7 +3,7 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         p1, p2 = 0, 0
-        while p1<len(s) and p2<len(t):
-            p1 += (s[p1]==t[p2])
+        while p1 < len(s) and p2 < len(t):
+            p1 += s[p1] == t[p2]
             p2 += 1
-        return p1==len(s)
+        return p1 == len(s)
