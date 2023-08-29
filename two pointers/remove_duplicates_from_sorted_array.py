3bb32a95c9fb1621af08b933719b00b3e4071b2b
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-29 16:35:21 +0530

    few changes

--- two pointers/remove_duplicates_from_sorted_array.py
+++ two pointers/remove_duplicates_from_sorted_array.py
@@ -1,9 +1,12 @@
+from typing import List
+
+
 # tc O(n), sc O(1)
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         l = 0
-        for r in range(1,len(nums)):
+        for r in range(1, len(nums)):
             if nums[l] != nums[r]:
                 l += 1
                 nums[l] = nums[r]
-        return l+1
+        return l + 1
--- two pointers/two_sum2_input_array_sorted.py
+++ two pointers/two_sum2_input_array_sorted.py
@@ -1,30 +1,33 @@
 from typing import List
 
-# binary search
-# tc O(n), sc O(1)
+
 class Solution:
-    def twoSum2(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers)-1
-        while l<r:
-            curSum = numbers[l]+numbers[r]
-            if curSum<target:
-                l+=1
-            elif curSum>target:
-                r-=1
+    # two pointers
+    # tc O(n), sc O(1)
+    def twoSum1(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            if curSum < target:
+                l += 1
+            elif curSum > target:
+                r -= 1
             else:
-                return [l+1, r+1]
+                return [l + 1, r + 1]
         return []
-    
-    def twoSum(self, numbers, target):
+
+    # binary search
+    # tc O(n), sc O(1)
+    def twoSum2(self, numbers, target):
         N = len(numbers)
         for i in range(N):
-            l, r = i+1, N-1
+            l, r = i + 1, N - 1
             tmp = target - numbers[i]
             while l <= r:
-                mid = l + (r-l)//2
+                mid = l + (r - l) // 2
                 if numbers[mid] == tmp:
-                    return [i+1, mid+1]
+                    return [i + 1, mid + 1]
                 elif numbers[mid] < tmp:
-                    l = mid+1
+                    l = mid + 1
                 else:
-                    r = mid-1
+                    r = mid - 1
