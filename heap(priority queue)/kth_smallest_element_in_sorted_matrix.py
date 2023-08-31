14b86a55e111c46bce979213b06f0d682bf1bcd4
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-31 16:30:31 +0530

    kth smallest element in sorted matrix

--- heap(priority queue)/kth_smallest_element_in_sorted_matrix.py
+++ heap(priority queue)/kth_smallest_element_in_sorted_matrix.py
@@ -1,8 +1,11 @@
 import heapq
-#maxheap
-# tc O(m*n*logk), sc O(k)
+from typing import List
+
+
 class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+    # maxheap
+    # tc O(m*n*logk), sc O(k)
+    def kthSmallest1(self, matrix: List[List[int]], k: int) -> int:
         ROWS = len(matrix)
         COLS = len(matrix[0])
         maxHeap = []
@@ -13,51 +16,46 @@ class Solution:
                     heapq.heappop(maxHeap)
 
         return -heapq.heappop(maxHeap)
-        
 
-# minHeap
-# tc O(k*logk), sc O(k)
-class Solution2:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+    # minHeap
+    # tc O(k*logk), sc O(k)
+    def kthSmallest2(self, matrix: List[List[int]], k: int) -> int:
         ROWS = len(matrix)
         COLS = len(matrix[0])
-        minHeap = [] # [value, row, col]
-        for r in range(min(k,ROWS)):
-            heapq.heappush(minHeap,[matrix[r][0],r,0])
-        
+        minHeap = []  # [value, row, col]
+        for r in range(min(k, ROWS)):
+            heapq.heappush(minHeap, [matrix[r][0], r, 0])
+
         res = -1
         for i in range(k):
             res, r, c = heapq.heappop(minHeap)
-            if (c+1) < COLS:
-                heapq.heappush(minHeap, [matrix[r][c+1], r, c+1])
+            if (c + 1) < COLS:
+                heapq.heappush(minHeap, [matrix[r][c + 1], r, c + 1])
         return res
-        
 
-# binary search
-# tc O(log(m*n)), sc O(1)
-class Solution3:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+    # binary search
+    # tc O(log(m*n)), sc O(1)
+    def kthSmallest3(self, matrix: List[List[int]], k: int) -> int:
         m = len(matrix)
         n = len(matrix[0])
 
         def getCountLessOrEqual(val):
             cnt = 0
-            c = n-1
+            c = n - 1
             for r in range(m):
-                while c>=0 and matrix[r][c]>val:
+                while c >= 0 and matrix[r][c] > val:
                     c -= 1
-                cnt += (c+1)
+                cnt += c + 1
             return cnt
 
         l = matrix[0][0]
         r = matrix[-1][-1]
         res = -1
         while l <= r:
-            mid = (l+r)//2
+            mid = (l + r) // 2
             if getCountLessOrEqual(mid) >= k:
                 res = mid
-                r = mid-1
+                r = mid - 1
             else:
-                l = mid+1
+                l = mid + 1
         return res
-        
