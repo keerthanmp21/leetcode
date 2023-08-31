f0e5f4736c5bce3067f1d4cab127038f14b3ae51
Keerthan M P <keerthankeeru775@gmail.com>
2023-08-31 07:11:16 +0530

    top k frequent elements

--- heap(priority queue)/top_k_frequent_elements.py
+++ heap(priority queue)/top_k_frequent_elements.py
@@ -1,26 +1,28 @@
-# bucket sort
-# tc O(n), sc O(n)
+from collections import Counter
+import heapq
+from typing import List
+
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = [[] for _ in range(len(nums)+1)]
+    # bucket sort
+    # tc O(n), sc O(n)
+    def topKFrequent1(self, nums: List[int], k: int) -> List[int]:
+        freq = [[] for _ in range(len(nums) + 1)]
         count = {}
         for num in nums:
-            count[num] = count.get(num,0)+1
+            count[num] = count.get(num, 0) + 1
         for num, cnt in count.items():
             freq[cnt].append(num)
         res = []
-        for i in range(len(freq)-1,-1,-1):
+        for i in range(len(freq) - 1, -1, -1):
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
 
-# heap(priority queue)
-# tc O(klogn), sc O(n)
-from collections import Counter
-import heapq
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    # heap(priority queue)
+    # tc O(klogn), sc O(n)
+    def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
         no_of_occurences = Counter(nums)
         maxHeap = [(-value, key) for key, value in no_of_occurences.items()]
         heapq.heapify(maxHeap)
