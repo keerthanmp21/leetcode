24fea32274c5a87a2833ef8069c606f6bbbbb822
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-04 06:36:28 +0530

    letter combination of a phone number

--- backtracking/letter_comb_of_a_phn_num.py
+++ backtracking/letter_comb_of_a_phn_num.py
@@ -1,25 +1,29 @@
 from typing import List
 
+
 class Solution:
     # tc O(3^n), sc O(n)
     def letterCombinations(self, digits: str) -> List[str]:
+        N = len(digits)
         res = []
-        digitToChar = {"2":"abc",
-        "3":"def",
-        "4":"ghi",
-        "5":"jkl",
-        "6":"mno",
-        "7":"pqrs",
-        "8":"tuv",
-        "9":"wxyz"}
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
 
         def backtrack(i, curStr):
-            if len(curStr) == len(digits):
+            if len(curStr) == N:
                 res.append(curStr)
                 return
             for char in digitToChar[digits[i]]:
-                backtrack(i+1, curStr+char)
+                backtrack(i + 1, curStr + char)
 
         if digits:
-            backtrack(0,'')
+            backtrack(0, "")
         return res
