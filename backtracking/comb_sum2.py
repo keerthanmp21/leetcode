197193e9eb39906fa8503ed6d43bca17007bbdd3
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-05 07:23:43 +0530

    comb sum 2

--- backtracking/comb_sum2.py
+++ backtracking/comb_sum2.py
@@ -1,24 +1,27 @@
 from typing import List
 
+
 class Solution:
     # tc O(2^n), sc O(n)
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        res=[]
+        res = []
         candidateLen = len(candidates)
+
         def backtrack(pos, cur, total):
             if total == target:
                 res.append(cur.copy())
                 return
             if total > target:
-                return 
+                return
             prev = -1
-            for i in range(pos,candidateLen):
+            for i in range(pos, candidateLen):
                 if candidates[i] == prev:
                     continue
                 cur.append(candidates[i])
-                backtrack(i+1, cur, total+candidates[i])
+                backtrack(i + 1, cur, total + candidates[i])
                 cur.pop()
                 prev = candidates[i]
+
         backtrack(0, [], 0)
         return res
