79ee3d479cd85bf45d4e7cd73582b8262b13c3cd
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-06 07:31:53 +0530

    subsets2

--- backtracking/subsets2.py
+++ backtracking/subsets2.py
@@ -1,23 +1,25 @@
 from typing import List
 
+
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
         N = len(nums)
-        
-        def backtrack(i,subset):
+
+        def backtrack(i, subset):
             if i == len(nums):
                 res.append(subset[::])
-                return 
-            
+                return
+
             subset.append(nums[i])
-            backtrack(i+1,subset)
+            backtrack(i + 1, subset)
             subset.pop()
-            
-            while i+1 < N and nums[i] == nums[i+1]:
-                i+=1
-            
-            backtrack(i+1,subset)
-        backtrack(0,[])
+
+            while i + 1 < N and nums[i] == nums[i + 1]:
+                i += 1
+
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
         return res
