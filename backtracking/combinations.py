1bff86c28bac7221f38d197e2d52a1ef9267b2ff
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-06 07:04:28 +0530

    combinations

--- backtracking/combinations.py
+++ backtracking/combinations.py
@@ -2,16 +2,18 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res=[]
-        def backtrack(start,comb):
-            if len(comb)==k:
+        res = []
+
+        def backtrack(start, comb):
+            if len(comb) == k:
                 res.append(comb.copy())
                 return
-            for i in range(start,n+1):
+            for i in range(start, n + 1):
                 comb.append(i)
-                backtrack(i+1,comb)
+                backtrack(i + 1, comb)
                 comb.pop()
-        backtrack(1,[])
+
+        backtrack(1, [])
         return res
     
 s = Solution()
