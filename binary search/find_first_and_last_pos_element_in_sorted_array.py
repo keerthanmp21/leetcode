cec50b9075e5c27600c1e261c1d107ad01e81e2b
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-07 07:24:07 +0530

    find first and last pos element in sorted array

--- binary search/find_first_and_last_pos_element_in_sorted_array.py
+++ binary search/find_first_and_last_pos_element_in_sorted_array.py
@@ -2,28 +2,47 @@ from typing import List
 
 # tc O(logn), sc O(1)
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
+    def searchRange1(self, nums: List[int], target: int) -> List[int]:
         def getLeft():
-            l, r = 0, len(nums)-1
-            while l<=r:
-                mid = (l+r)//2
-                if target>nums[mid]:
-                    l += 1
+            l, r = 0, len(nums) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if target > nums[mid]:
+                    l = mid + 1
                 else:
-                    r -= 1
+                    r = mid - 1
             return l
 
         def getRight():
-            l, r = 0, len(nums)-1
-            while l<=r:
-                mid = (l+r)//2
-                if target>=nums[mid]:
-                    l += 1
+            l, r = 0, len(nums) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if target >= nums[mid]:
+                    l = mid + 1
                 else:
-                    r -= 1
+                    r = mid - 1
             return r
 
-        left ,right = getLeft(), getRight()
-        if left<=right:
+        left, right = getLeft(), getRight()
+        if left <= right:
             return [left, right]
-        return [-1,-1]
+        return [-1, -1]
+
+    def searchRange2(self, nums: List[int], target: int) -> List[int]:
+        def lower_bound(target2):
+            l, r = 0, len(nums) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] < target2:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return l
+
+        lb1 = lower_bound(target)
+        lb2 = lower_bound(target + 1) - 1
+
+        if lb1 < len(nums) and nums[lb1] == target:
+            return [lb1, lb2]
+        else:
+            return [-1, -1]
