d5197efe15e97e2d0734c0e008b21a41e55876fd
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-15 21:25:03 +0530

    longest zig zag path in bt

--- trees/longest_zig_zag_path_in_bt.py
+++ trees/longest_zig_zag_path_in_bt.py
@@ -6,15 +6,23 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         self.maxLength = 0
+
         def dfs(node, deep, dir):
             self.maxLength = max(self.maxLength, deep)
 
             if node.left is not None:
-                dfs(node.left, deep+1,'left') if dir != 'left' else dfs(node.left, 1, 'left')
+                dfs(node.left, deep + 1, "left") if dir != "left" else dfs(
+                    node.left, 1, "left"
+                )
             if node.right is not None:
-                dfs(node.right, deep+1, 'right') if dir != 'right' else dfs(node.right, 1, 'right')
-        dfs(root, 0, '')
+                dfs(node.right, deep + 1, "right") if dir != "right" else dfs(
+                    node.right, 1, "right"
+                )
+
+        dfs(root, 0, "")
         return self.maxLength
