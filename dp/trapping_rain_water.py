869974e8d4d9d537305e13c42b11e89bb333f782
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-21 07:14:29 +0530

    trapping rain water

--- dp/trapping_rain_water.py
+++ dp/trapping_rain_water.py
@@ -1,21 +1,21 @@
 class Solution:
-    #two pointers
-    #tc O(n), sc O(1)
+    # two pointers
+    # tc O(n), sc O(1)
     def trap(self, height):
         if not height:
             return 0
-        l,r = 0, len(height)-1
+        l, r = 0, len(height) - 1
         leftMax, rightMax = height[l], height[r]
         res = 0
-        while l<r:
+        while l < r:
             if leftMax < rightMax:
-                l+=1
+                l += 1
                 leftMax = max(leftMax, height[l])
-                res += leftMax-height[l]
+                res += leftMax - height[l]
             else:
-                r-=1
+                r -= 1
                 rightMax = max(rightMax, height[r])
-                res += rightMax-height[r]
+                res += rightMax - height[r]
         return res
 
     # monotonic stack
@@ -23,15 +23,15 @@ class Solution:
     def trap2(self, height):
         stack = []
         traps = 0
-        #monotonic decreasing stack
+        # monotonic decreasing stack
         for i, h in enumerate(height):
-            while stack and h>height[stack[-1]]:
+            while stack and h > height[stack[-1]]:
                 bottom = stack.pop()
                 if stack:
                     minHeight = min(h, height[stack[-1]])
-                    trapHeight = minHeight-height[bottom]
-                    width = i-stack[-1]-1
-                    traps += trapHeight*width
+                    trapHeight = minHeight - height[bottom]
+                    width = i - stack[-1] - 1
+                    traps += trapHeight * width
 
             stack.append(i)
         return traps
@@ -48,31 +48,31 @@ class Solution:
                 leftMax = max(leftMax, height[j])
 
             rightMax = 0
-            for j in range(i+1,N):
+            for j in range(i + 1, N):
                 rightMax = max(rightMax, height[j])
-            
-            curTrap = min(leftMax, rightMax)-height[i]
-            traps += curTrap if curTrap>0 else 0
+
+            curTrap = min(leftMax, rightMax) - height[i]
+            traps += curTrap if curTrap > 0 else 0
 
         return traps
-        
+
     # dp iterative
     # tc O(n), sc O(n)
     def trap4(self, height):
         N = len(height)
-        if N<=2:
+        if N <= 2:
             return 0
-        leftMax, rightMax = [0]*N, [0]*N
-        leftMax[0], rightMax[N-1] = height[0], height[N-1]
+        leftMax, rightMax = [0] * N, [0] * N
+        leftMax[0], rightMax[N - 1] = height[0], height[N - 1]
 
-        for i in range(1,N):
-            leftMax[i] = max(leftMax[i-1],height[i])
+        for i in range(1, N):
+            leftMax[i] = max(leftMax[i - 1], height[i])
 
-        for i in range(N-2,-1,-1):
-            rightMax[i] = max(rightMax[i+1],height[i])
+        for i in range(N - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
 
         traps = 0
         for i in range(N):
-            traps += min(leftMax[i], rightMax[i]) -height[i]
+            traps += min(leftMax[i], rightMax[i]) - height[i]
 
         return traps
