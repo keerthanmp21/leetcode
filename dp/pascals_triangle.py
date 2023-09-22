cf20e9f744c3ca8991ed7d2d77969af1edcdc6c1
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-22 21:36:51 +0530

    pascals triangle

--- dp/pascals_triangle.py
+++ dp/pascals_triangle.py
@@ -1,16 +1,18 @@
 from typing import List
 
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         res = [[1]]
-        
-        for _ in range(numRows-1):
+
+        for _ in range(numRows - 1):
             temp = [0] + res[-1] + [0]
             row = []
-            for i in range(len(res[-1])+1):
-                row.append(temp[i]+temp[i+1])
+            for i in range(len(res[-1]) + 1):
+                row.append(temp[i] + temp[i + 1])
             res.append(row)
         return res
-    
+
+
 s = Solution()
 print(s.generate(5))
