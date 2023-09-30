6e69b2ce61e6686076eb65bd8eb7f4692411434f
Keerthan M P <keerthankeeru775@gmail.com>
2023-09-30 07:16:54 +0530

    car fleet

---
+++ stack/car_fleet.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p, s] for p, s in zip(position, speed)]
+        stack = []
+
+        # [::-1] coz car speed depends on next car so its better to calculate it from last
+        for p, s in sorted(pair)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
