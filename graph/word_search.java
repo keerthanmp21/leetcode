14c9c1ad7ca7c52a143a717f3453314a1bd1a5b5
Keerthan M P <keerthankeeru775@gmail.com>
2024-01-27 21:20:04 +0530

    word search

---
+++ graph/word_search.java
@@ -0,0 +1,34 @@
+class Solution {
+    private int ROWS, COLS;
+    private Set<String> path;
+    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public boolean exist(char[][] board, String word) {
+        ROWS = board.length;
+        COLS = board[0].length;
+        path = new HashSet<>();
+        for(int r = 0; r < ROWS; r++){
+            for(int c = 0; c < COLS; c++){
+                if(dfs(board, r, c, word, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(char[][] board, int r, int c, String word, int i){
+        if(i == word.length()){
+            return true;
+        }
+        if(r < 0 || c < 0 || r == ROWS || c == COLS || word.charAt(i) != board[r][c] || path.contains(r + "," + c)){
+            return false;
+        }
+        path.add(r + "," + c);
+        boolean res = false;
+        for(int[] dir : directions){
+            res = res || dfs(board, r + dir[0], c + dir[1], word, i + 1);
+        }
+        path.remove(r + "," + c);
+        return res;
+    }
+}
