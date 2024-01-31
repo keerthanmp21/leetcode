4364dc0d5a91532cd9a4ca173d504d143c20f5aa
Keerthan M P <keerthankeeru775@gmail.com>
2024-01-31 15:44:28 +0530

    max_area_of_islands java

--- graph/max_area_of_island.py
+++ graph/max_area_of_island.py
@@ -4,25 +4,25 @@ from collections import deque
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         self.ROWS, self.COLS = len(grid), len(grid[0])
-        self.directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        self.directions = [[1, 0],[-1, 0],[0, 1],[0, -1]]
         self.visited = set()
         maxSize = 0
 
         for r in range(self.ROWS):
             for c in range(self.COLS):
-                size = self.dfs(r,c,grid)
+                size = self.dfs(r, c, grid)
                 maxSize = max(maxSize, size)
         
         return maxSize
 
 
     def dfs(self, r, c, grid):
-        if (r<0 or r==self.ROWS) or (c<0 or c==self.COLS) or (r,c) in self.visited or grid[r][c] == 0:
+        if (r < 0 or r == self.ROWS) or (c < 0 or c == self.COLS) or (r, c) in self.visited or grid[r][c] == 0:
             return 0
-        self.visited.add((r,c))
+        self.visited.add((r, c))
         size = 1
         for d in self.directions:
-            size += self.dfs(r+d[0], c+d[1], grid)
+            size += self.dfs(r + d[0], c + d[1], grid)
         return size
     
     # bfs
@@ -30,19 +30,19 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         visited = set()
         maxSize = 0
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        directions = [[1, 0],[-1, 0],[0, 1],[0, -1]]
 
         def bfs(r,c):
-            if (r<0 or r==ROWS) or (c<0 or c==COLS) or (r,c) in visited or grid[r][c] == 0:
+            if (r < 0 or r == ROWS) or (c < 0 or c == COLS) or (r, c) in visited or grid[r][c] == 0:
                 return 0
             q = deque()
-            q.append((r,c))
+            q.append((r, c))
             size = 1
             while q:
                 r,c = q.popleft()
-                visited.add((r,c))
+                visited.add((r, c))
                 for d in directions:
-                    size += bfs(r+d[0], c+d[1])
+                    size += bfs(r + d[0], c + d[1])
                 
             return size
 
---
+++ graph/max_area_of_islands.java
@@ -0,0 +1,67 @@
+public class max_area_of_islands {
+    
+}
+
+class Solution {
+    private int ROWS;
+    private int COLS;
+    private int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private Set<String> visited = new HashSet<>();
+    
+    public int maxAreaOfIsland1(int[][] grid) {
+        ROWS = grid.length;
+        COLS = grid[0].length;
+        int maxSize = 0;
+        for(int r = 0; r < ROWS; r++){
+            for(int c = 0; c < COLS; c++){
+                maxSize = Math.max(maxSize, dfs(r, c, grid));
+            }
+        }
+        return maxSize;
+    }
+
+    private int dfs(int r, int c, int[][] grid){
+        if(r < 0 || r == ROWS || c < 0 || c == COLS || visited.contains(r + "," + c) || grid[r][c] == 0){
+            return 0;
+        }
+        visited.add(r + "," + c);
+        int size = 1;
+        for(int[] d : directions){
+            size += dfs(r + d[0], c + d[1], grid);
+        }
+        return size;
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        ROWS = grid.length;
+        COLS = grid[0].length;
+        int maxSize = 0;
+        for(int r = 0; r < ROWS; r++){
+            for(int c = 0; c < COLS; c++){
+                maxSize = Math.max(maxSize, dfs(r, c, grid));
+            }
+        }
+        return maxSize;
+    }
+
+    private int bfs(int r, int c, int[][] grid){
+        if(r < 0 || r == ROWS || c < 0 || c == COLS || visited.contains(r + "," + c) || grid[r][c] == 0){
+            return 0;
+        }
+        Deque<int[]> deque = new LinkedList<>();
+        deque.add(new int[]{r, c});
+        int size = 1;
+
+        while(!deque.isEmpty()){
+            int[] cell = deque.poll();
+            int row = cell[0];
+            int col = cell[1];
+            visited.add(r + "," + c);
+            for(int[] d : directions){
+                size += bfs(row + d[0], col + d[1], grid);
+            }
+        }
+
+        return size;
+    }
+}
