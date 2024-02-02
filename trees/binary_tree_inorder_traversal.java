84eec2292753845e43bbdee53ca19e766b783c68
Keerthan M P <keerthankeeru775@gmail.com>
2024-02-02 19:31:06 +0530

    bt_inorder_traversal java

--- trees/binary_tree_inorder_traversal.java
+++ trees/binary_tree_inorder_traversal.java
@@ -20,7 +20,7 @@ public class TreeNode {
 }
 
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversal1(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         dfs(root, result);
         return result;
@@ -33,5 +33,22 @@ class Solution {
         dfs(root.left, result);
         result.add(root.val);
         dfs(root.right, result);
+    }
+
+    public List<Integer> inorderTraversal2(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+
+        return res;
     }
 }
