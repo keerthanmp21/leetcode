15ef92361e557785861142ad8d2df119b31bb357
Keerthan M P <keerthankeeru775@gmail.com>
2024-02-14 07:43:38 +0530

    sliding window maximum

--- heap(priority queue)/sliding_window_maximum.py
+++ heap(priority queue)/sliding_window_maximum.py
@@ -1,16 +1,42 @@
 import heapq
+from typing import List
+import collections
+
 class Solution:
+    def maxSlidingWindow1(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = collections.deque()
+        l = r = 0
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+            
+            if l > q[0]:
+                q.popleft()
+                
+            if (r+1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+        return output
+    
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        if not nums or k == 1:
-            return nums
         N = len(nums)
+        if N == 0 or k == 1:
+            return nums
         maxHeap = []
         for i in range(k): # first window of k size
-            heapq.heappush(maxHeap, (-nums[i],i))
-        res = [-maxHeap[0][0]] # add max value of first window
-        for i in range(k,N): # start from second window
-            while maxHeap[0][1] <= i-k: # outer bound
+            heapq.heappush(maxHeap, [-nums[i], i])
+        
+        res = []
+        res.append(-maxHeap[0][0]) # add max value of first window
+
+        for i in range(k, N): # start from second window
+            while maxHeap[0][1] <= i - k: # outer bound
                 heapq.heappop(maxHeap)
-            heapq.heappush(maxHeap, (-nums[i],i))
+            heapq.heappush(maxHeap, [-nums[i], i])
             res.append(-maxHeap[0][0])
+
         return res
+        
