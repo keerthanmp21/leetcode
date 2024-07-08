403bfd1a75987b3a9caeaaaffad13efb125d7848
Keerthan M P <keerthankeeru775@gmail.com>
2024-07-08 06:50:46 +0530

    daily_temperature.java

--- stack/daily_temperature.java
+++ stack/daily_temperature.java
@@ -3,14 +3,38 @@ package stack;
 import java.util.Stack;
 
 public class daily_temperature {
-    public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stk = new Stack<>();
-        int []res = new int[temperatures.length];
+    / brute force (tle)
+    // time O(n^2), space O(n)
+    public int[] dailyTemperatures1(int[] temperatures) {
+        int N = temperatures.length;
+        int[] res = new int[N];
 
-        for(int i=0; i<temperatures.length; i++){
-            while(!stk.isEmpty() && temperatures[i]>temperatures[stk.peek()]){
+        for(int i = 0; i < N; i++){
+            int t1 = temperatures[i];
+            for(int j = i + 1; j < N; j++){
+                int t2 = temperatures[j];
+                if(t2 > t1){
+                    res[i] = (j - i);
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+
+        // monotonic decreasing stack
+    // time O(n), space O(n)
+    public int[] dailyTemperatures(int[] temperatures){
+        int N = temperatures.length;
+        int[] res = new int[N];
+        Stack<Integer> stk = new Stack<>();
+
+        for(int i = 0; i < N; i++){
+            int curT = temperatures[i];
+            while(!stk.isEmpty() && curT > temperatures[stk.peek()]){
                 int topInd = stk.pop();
-                res[topInd] = i-topInd;
+                res[topInd] = i - topInd;
             }
             stk.push(i);
         }
