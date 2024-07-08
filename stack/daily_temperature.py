092c84bbac8413d59adc12954a6910a4edc7387d
Keerthan M P <keerthankeeru775@gmail.com>
2024-07-08 06:50:05 +0530

    daily_temperature.py

--- stack/daily_temperature.py
+++ stack/daily_temperature.py
@@ -2,15 +2,33 @@ from typing import List
 
 
 class Solution:
+    # brute force (tle)
+    # time O(n^2), space O(n)
+    def dailyTemperatures1(self, temperatures: List[int]) -> List[int]:
+        N = len(temperatures)
+        res = [0] * N
+
+        for i in range(N):
+            t1 = temperatures[i]
+            for j in range(i + 1, N):
+                t2 = temperatures[j]
+                if t2 > t1:
+                    res[i] = (j - i)
+                    break
+
+        return res
+    
+    # monotonic decreasing stack
+    # time O(n), space O(n)
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = [0] * len(temperatures)
+        N = len(temperatures)
+        res = [0] * N
         stack = []
 
         for i, t in enumerate(temperatures):
-            # monotonic decreasing
-            while stack and t > stack[-1][-1]:
-                topInd, _ = stack.pop()
-                res[topInd] = i - topInd
+            while stack and t > stack[-1][1]:
+                topInd, topTemp = stack.pop()
+                res[topInd] = (i - topInd)
             stack.append([i, t])
 
         return res
