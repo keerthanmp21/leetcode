5c3ffcad3695c394d49aebfe7a49be97a9f2b421
Keerthan M P <keerthankeeru775@gmail.com>
2025-02-26 12:21:01 +0530

    find_peak_element.py

--- binary search/find_peak_element.py
+++ binary search/find_peak_element.py
@@ -2,7 +2,7 @@ from typing import List
 
 # tc O(logn), sc O(1)
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
+    def findPeakElement1(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
         while l < r:
             mid = (l + r) // 2
@@ -13,6 +13,17 @@ class Solution:
             else:
                 r = mid - 1
         return l
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] < nums[mid + 1]:  # Peak must be on the right
+                l = mid + 1
+            else:  # Peak is on the left or mid is the peak
+                r = mid
+        return l  # or return r (both point to the peak)
+
 
                 
         
