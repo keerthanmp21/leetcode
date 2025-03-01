b055cacee7ca2de8cb911c75b4d50d42f99055cb
Keerthan M P <keerthankeeru775@gmail.com>
2025-03-01 12:37:21 +0530

    set_matrix_zeroes

--- matrix/set_matrix_zeroes.py
+++ matrix/set_matrix_zeroes.py
@@ -1,78 +1,46 @@
 from typing import List
 
 class Solution:
-    # backtrack
-    # tc = O(2^(m+n)), sc = O(m+n)
-    def uniquePathsWithObstacles1(self, obstacleGrid: List[List[int]]) -> int:
-        if obstacleGrid[-1][-1]:
-            return 0
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
+    def setZeroes1(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
 
-        def backtrack(r, c):
-            if r == m - 1 and c == n - 1:
-                return 1
-            if r < 0 or r >= m or c < 0 or c >= n or obstacleGrid[r][c]:
-                return 0
-            return backtrack(r + 1, c) + backtrack(r, c + 1)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
 
-        return backtrack(0, 0)
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
 
-    # dp memoization
-    # tc O(m*n), sc O(m*n)
-    def uniquePathsWithObstacles2(self, obstacleGrid: List[List[int]]) -> int:
-        if obstacleGrid[-1][-1]:
-            return 0
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-        dp = [[0] * n for _ in range(m)]
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
 
-        def backtrack(r, c):
-            if r == m - 1 and c == n - 1:
-                return 1
-            if r < 0 or r >= m or c < 0 or c >= n or obstacleGrid[r][c]:
-                return 0
-            if dp[r][c]:
-                return dp[r][c]
-            dp[r][c] = backtrack(r + 1, c) + backtrack(r, c + 1)
-            return dp[r][c]
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
 
-        return backtrack(0, 0)
+    def setZeroes2(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowSet, colSet = set(), set()
 
-    # dp tabulation
-    # tc O(m*n), sc O(m*n)
-    def uniquePathsWithObstacles3(self, obstacleGrid: List[List[int]]) -> int:
-        if obstacleGrid[-1][-1]:
-            return 0
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        dp[0][1] = 1
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                dp[i][j] = (
-                    dp[i - 1][j] + dp[i][j - 1] if not obstacleGrid[i - 1][j - 1] else 0
-                )
-        return dp[-1][-1]
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    rowSet.add(r)
+                    colSet.add(c)
 
-    # in-place
-    # tc O(m*n), O(1)
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        if obstacleGrid[-1][-1]:
-            return 0
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-        obstacleGrid[0][0] ^= 1
-        for i in range(1, m):
-            obstacleGrid[i][0] = obstacleGrid[i - 1][0] & (not obstacleGrid[i][0])
-
-        for i in range(1, n):
-            obstacleGrid[0][i] = obstacleGrid[0][i - 1] & (not obstacleGrid[0][i])
-
-        for i in range(1, m):
-            for j in range(1, n):
-                obstacleGrid[i][j] = (
-                    (obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1])
-                    if not obstacleGrid[i][j]
-                    else 0
-                )
-
-        return obstacleGrid[m - 1][n - 1]
+        for r in range(ROWS):
+            for c in range(COLS):
+                if r in rowSet or c in colSet:
+                    matrix[r][c] = 0
