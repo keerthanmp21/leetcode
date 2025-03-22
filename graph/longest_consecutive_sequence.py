ee03e0e5ad63f2402aa219306835e359b38945e8
Keerthan M P <keerthankeeru775@gmail.com>
2025-03-22 11:25:39 +0530

    longest_consecutive_sequence.py

--- graph/longest_consecutive_sequence.py
+++ graph/longest_consecutive_sequence.py
@@ -1,9 +1,50 @@
 from typing import List
 
+class UnionFind:
+    def __init__(self, nums):
+        self.parent = {num: num for num in nums}
+        self.rank = {num:1 for num in nums}
+
+    def find(self, num):
+        # if self.parent[num] != num:
+        #     self.parent[num] = self.find(self.parent[num])  # Path compression
+        # return self.parent[num]
+
+        while num != self.parent[num]:  
+            self.parent[num] = self.parent[self.parent[num]]  # Path compression (flatten tree)
+            num = self.parent[num]  # Move up the tree
+        return num
+
+    def union(self, a, b):
+        rootA = self.find(a)
+        rootB = self.find(b)
+        if rootA != rootB:  # If they are not already connected
+            if self.rank[rootA] > self.rank[rootB]:
+                self.parent[rootB] = rootA
+                self.rank[rootA] += self.rank[rootB]  # Increase size of rootX
+            else:
+                self.parent[rootA] = rootB
+                self.rank[rootB] += self.rank[rootA]  # Increase size of rootY
+
+
 class Solution:
     # brute force
     # time O(n * (n or len of longest)), space O(n)
     def longestConsecutive1(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for num in nums:
+            length = 0
+            while (num + length) in nums:
+                length += 1
+            longest = max(longest, length)
+
+        return longest
+
+    # brute force
+    # time O(n * (n or len of longest)), space O(n)
+    def longestConsecutive2(self, nums: List[int]) -> int:
         numSet = set(nums)
         longest = 0
 
@@ -18,7 +59,7 @@ class Solution:
 
     # union find
     # time O(n), space O(n)
-    def longestConsecutive2(self, nums: List[int]) -> int:
+    def longestConsecutive3(self, nums: List[int]) -> int:
         if not nums:
             return 0
         node = {} #value = parent node
@@ -44,5 +85,16 @@ class Solution:
         #print(rank)
         return max(rank.values())
             
+
+    # time complexity O(N)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        uf = UnionFind(nums)
+
+        for num in nums:
+            if (num + 1) in uf.parent:
+                uf.union(num, num + 1)
             
-            
+        return max(uf.rank.values(), default = 0)
