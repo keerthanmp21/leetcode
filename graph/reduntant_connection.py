b52c797d2bef16c7e9eade5af4f38a46a08929b4
Keerthan M P <keerthankeeru775@gmail.com>
2025-03-22 13:02:10 +0530

    reduntant_connection.py union find

--- graph/reduntant_connection.py
+++ graph/reduntant_connection.py
@@ -1,10 +1,40 @@
 from typing import List
 from collections import deque, defaultdict
 
+class UnionFind:
+    def __init__(self, n):
+        self.parent = {i: i for i in range(1, n + 1)}
+        self.rank = {i: 0 for i in range(1, n + 1)}
+
+    def find(self, num):
+        while num != self.parent[num]:
+            self.parent[num] = self.parent[self.parent[num]]
+            num = self.parent[num]
+        return num # self.parent[num]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX == rootY:
+            return False # cycle detected
+
+        if rootX != rootY:
+            # Union by rank
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+
+        return True
+
 class Solution:
     # union find
     # time O(n), space O(n)
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+    def findRedundantConnection1(self, edges: List[List[int]]) -> List[int]:
         parent = [i for i in range(len(edges) + 1)]
         weight = [1 for i in range(len(edges) + 1)]
 
@@ -94,5 +124,16 @@ class Solution:
                 return [src, dest]
             edges_list[src].append(dest)
             edges_list[dest].append(src)
+
+        return []
+
+    # Time Complexity: O(N) (almost constant due to path compression).
+    # Space Complexity: O(N) (storing parents and ranks).
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind(len(edges))
+
+        for src, dest in edges:
+            if not uf.union(src, dest):
+                return [src, dest]
 
         return []
