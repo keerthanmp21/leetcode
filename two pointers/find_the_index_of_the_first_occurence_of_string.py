2227b301207049eab201ef5c26f00eb4c58ca03a
Keerthan M P <keerthankeeru775@gmail.com>
2025-03-26 16:15:45 +0530

    kmp algo

--- two pointers/find_the_index_of_the_first_occurence_of_string.py
+++ two pointers/find_the_index_of_the_first_occurence_of_string.py
@@ -12,7 +12,7 @@ class Solution:
 
     # two pointers
     # tc O(m), sc O(1)
-    def strStr2(self, haystack: str, needle: str) -> int:
+    def strStr(self, haystack: str, needle: str) -> int:
         l1, l2, r1, r2 = 0, 0, len(haystack), len(needle)
         while l1 < r1:
             if l2 == r2:
@@ -26,3 +26,44 @@ class Solution:
         if l2 == r2:
             return l1 - l2
         return -1
+
+    def strStr3(self, haystack: str, needle: str) -> int:
+        def compute_lps(pattern):
+            """Computes the LPS (Longest Prefix Suffix) array."""
+            m = len(pattern)
+            lps = [0] * m
+            j = 0  # Length of previous longest prefix suffix
+
+            for i in range(1, m):
+                while j > 0 and pattern[i] != pattern[j]:
+                    j = lps[j - 1]
+
+                if pattern[i] == pattern[j]:
+                    j += 1
+                    lps[i] = j
+
+            return lps
+
+        def kmp_search(text, pattern):
+            """Searches for occurrences of 'pattern' in 'text' using KMP algorithm."""
+            n, m = len(text), len(pattern)
+            lps = compute_lps(pattern)
+
+            j = 0  # Pointer for pattern
+            occurrences = []
+
+            for i in range(n):
+                while j > 0 and text[i] != pattern[j]:
+                    j = lps[j - 1]
+
+                if text[i] == pattern[j]:
+                    j += 1
+                
+                if j == m:
+                    return (i - m + 1)
+                    
+
+            return -1
+
+        res = kmp_search(haystack, needle)
+        return res
